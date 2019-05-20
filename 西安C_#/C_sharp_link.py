5aa42a5ce01780648a0c5f8f6fff6cf473bdd754
Greenbirch2007 <291109028@qq.com>
2019-05-20 19:39:32 +0800

    增加c#

--- 西安C_#/C_sharp_link.py
+++ 西安C_#/C_sharp_link.py
@@ -15,11 +15,16 @@ def get_first_page():
     driver.get(url)
     time.sleep(3)
     driver.find_element_by_xpath('//*[@id="loginname"]').clear()
-    driver.find_element_by_xpath('//*[@id="loginname"]').send_keys("")#用户名
+    driver.find_element_by_xpath('//*[@id="loginname"]').send_keys("") #用户名
     driver.find_element_by_xpath('//*[@id="password"]').clear()
     driver.find_element_by_xpath('//*[@id="password"]').send_keys("")#密码
     driver.find_element_by_xpath('//*[@id="login_btn"]').click()
-    driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[1]/div[2]/div[2]/a').click()
+    driver.find_element_by_xpath('//*[@id="topIndex"]/div/p/a[2]').click()
+    driver.find_element_by_xpath('//*[@id="kwdselectid"]').send_keys("C#")  #可以针对其他岗位进行统计分析
+    # 还要剔除本地选项　　（“”）
+
+    driver.find_element_by_xpath('/html/body/div[2]/form/div/div[1]/button').click()
+
     html = driver.page_source
     return html
 
@@ -30,8 +35,8 @@ def get_first_page():
 # 把首页和翻页处理？
 
 def next_page():
-    for i in range(1,666):  # selenium 循环翻页成功！
-        driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div[3]/div/div/div/ul/li[last()]/a').click()
+    for i in range(1,26):  # selenium 循环翻页成功！
+        driver.find_element_by_xpath('//*[@id="resultList"]/div[55]/div/div/div/ul/li[last()]/a').click()
         time.sleep(1)
         html = driver.page_source
         return html
@@ -41,15 +46,10 @@ def next_page():
 def parse_html(html):  # 正则专门有反爬虫的布局设置，不适合爬取表格化数据！
     big_list = []
     selector = etree.HTML(html)
-    jobs = selector.xpath('/html/body/div[2]/div[2]/div[2]/div[2]/div/div/a[1]/text()')
-    job_link  =selector.xpath('/html/body/div[2]/div[2]/div[2]/div[2]/div/div/a[1]/@href')
-    firms = selector.xpath('/html/body/div[2]/div[2]/div[2]/div[2]/div/div/a[2]/text()')
-    salary = selector.xpath('/html/body/div[2]/div[2]/div[2]/div[2]/div/div/span[1]/text()')
-    nums = selector.xpath('/html/body/div[2]/div[2]/div[2]/div[2]/div/div[3]/div/span/text()')
-    page_nums = selector.xpath('//*[@id="jump_page"]/@value')
-    f_page_nums = len(jobs)*page_nums
-
-    long_tuple = (i for i in zip(jobs, job_link,firms, salary, nums,f_page_nums))
+    jobs = selector.xpath("//div[@class='dw_table']/div/p/span[1]/a/@title")
+    link = selector.xpath("//div[@class='dw_table']/div/p/span[1]/a/@href")
+    firms = selector.xpath('//*[@id="resultList"]/div/span[1]/a/text()')
+    long_tuple = (i for i in zip(jobs, link, firms))
     for i in long_tuple:
         big_list.append(i)
     return big_list
@@ -63,7 +63,7 @@ def insertDB(content):
                                  charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
     cursor = connection.cursor()
     try:
-        cursor.executemany('insert into QCWY_NUM (jobs,job_link,firms,salary,nums,f_page_nums) values (%s,%s,%s,%s,%s,%s)', content)
+        cursor.executemany('insert into C_sharp_link (jobs,link,firms) values (%s,%s,%s)', content)
         connection.commit()
         connection.close()
         print('向MySQL中添加数据成功！')
@@ -76,7 +76,6 @@ def insertDB(content):
 
 if __name__ == '__main__':
         html = get_first_page()
-        time.sleep(1)
         content = parse_html(html)
         time.sleep(1)
         insertDB(content)
@@ -85,18 +84,13 @@ if __name__ == '__main__':
             content = parse_html(html)
             insertDB(content)
             print(datetime.datetime.now())
+            time.sleep(1)
 
 
 # #
-# create table QCWY_NUM(
+# create table C_sharp_link(
 # id int not null primary key auto_increment,
-# jobs varchar(50),
-# job_link varchar(100),
-# firms varchar(80),
-# salary varchar(16),
-# nums varchar(8),
-# f_page_nums varchar(6)
+# jobs varchar(80),
+# link varchar(88),
+# firms varchar(80)
 # ) engine=InnoDB  charset=utf8;
-
-# drop table QCWY_NUM;
-
